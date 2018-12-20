5ba08916ff50e86893b37550d03fa023807fff77
vv258 <vv258@cornell.edu>
2018-12-20 02:00:46 -0500

    v2

--- Srobot.c
+++ Srobot.c
@@ -30,7 +30,7 @@ static int timerlimit,  entry_time, execution_time, isr_time;
 static int pwm_on_time_wheel1 =0,pwm_on_time_wheel2=0, pwm_on_time_idle=0,pwm_on_time_max=0,pwm_on_time_min=0;
 static int pwm_on_time_1 =0, pwm_on_time_2=0;
 
-static int kp=0.5,kd=3;
+static int kp=1,kd=3;
 static int proportional_cntl_1=0,differential_cntl_1=0,integral_cntl_1=0;
 static int proportional_cntl_2=0,differential_cntl_2=0,integral_cntl_2=0;
 
@@ -41,7 +41,7 @@ static int tdoa1_error=0, tdoa1_last_error=0;
 static int tdoa2_error=0, tdoa2_last_error=0;
 static int tdoa1_cmd=0, tdoa2_cmd=0, tdoa1_threshold=0;
 static struct pt pt_display,  pt_timer;
-
+char buffer[50];
 
 void printLine(int line_number, char* print_buffer, short text_color, short back_color){
     // line number 0 to 31 
@@ -81,6 +81,8 @@ static PT_THREAD (protothread_display(struct pt *pt))
     printLine(14, buffer, ILI9340_WHITE, ILI9340_BLUE);
     sprintf(buffer,"time13 = %d", time13);
     printLine(16, buffer, ILI9340_WHITE, ILI9340_BLUE);
+    sprintf(buffer,"sys time = %d", sys_time_seconds);
+    printLine(18, buffer, ILI9340_WHITE, ILI9340_BLUE);
     PT_YIELD_TIME_msec(100) ;
   
     PT_END(pt);
@@ -139,7 +141,7 @@ time13=time1-time3;
             differential_cntl_1 = kd * (tdoa1_error - tdoa1_last_error); 
             integral_cntl_1 = integral_cntl_1 + tdoa1_error ;
             
-            if((tdoa1_error<0 && tdoa1_last_error>0) ||(tdoa1_error>0 &&t tdoa1_last_error<0))
+            if((tdoa1_error<0 && tdoa1_last_error>0) ||(tdoa1_error>0 && tdoa1_last_error<0))
                     integral_cntl_1=90*integral_cntl_1/100;
 
             pwm_on_time_1 =  proportional_cntl_1 + differential_cntl_1 + ki * integral_cntl_1 ;
@@ -153,7 +155,7 @@ time13=time1-time3;
             differential_cntl_2 = kd * (tdoa2_error - tdoa2_last_error); 
             integral_cntl_2 = integral_cntl_2 + tdoa2_error ;
             
-            if((tdoa2_error<0 && tdoa2_last_error>0) ||(tdoa2_error>0 &&t tdoa2_last_error<0))
+            if((tdoa2_error<0 && tdoa2_last_error>0) ||(tdoa2_error>0 && tdoa2_last_error<0))
                     integral_cntl_2=90*integral_cntl_2/100;
             
             pwm_on_time_2 =  proportional_cntl_2 + differential_cntl_2 + ki * integral_cntl_2 ;
@@ -173,8 +175,10 @@ time13=time1-time3;
     else if(pwm_on_time_wheel2>pwm_on_time_max)
         pwm_on_time_wheel2=pwm_on_time_max;
     
-    SetDCOC3PWM(pwm_on_time_wheel1);
-    SetDCOC2PWM(pwm_on_time_wheel2);
+   // SetDCOC3PWM(pwm_on_time_wheel1);
+   // SetDCOC2PWM(pwm_on_time_wheel2);
+ SetDCOC3PWM(timerlimit-pwm_on_time_max);
+    SetDCOC2PWM(timerlimit-pwm_on_time_min);
 
 
 execution_time = ReadTimer2() ; //  execution time for ISR
@@ -182,41 +186,55 @@ isr_time= entry_time+ execution_time;
      
 } // end ISR TIMER2
 void __ISR(_EXTERNAL_3_VECTOR, ipl7) INT3Interrupt() 
-{  
-   WriteTimer3(0x000);
-   OpenCapture1(  IC_EVERY_RISE_EDGE | IC_INT_1CAPTURE | IC_TIMER3_SRC | IC_ON );
-   OpenCapture2(  IC_EVERY_RISE_EDGE | IC_INT_1CAPTURE | IC_TIMER3_SRC | IC_ON );
-   OpenCapture3(  IC_EVERY_RISE_EDGE | IC_INT_1CAPTURE | IC_TIMER3_SRC | IC_ON );
-   mINT3ClearIntFlag(); 
+{  //
+//   WriteTimer3(0x000);
+    mINT3ClearIntFlag(); 
+   delay_ms(20);
+   
 } 
 void __ISR(_INPUT_CAPTURE_1_VECTOR, ipl3) C1Handler(void)
 {
     // read the capture register 
     time1 = mIC1ReadCapture();
-    CloseCapture1();
+      mIC1ClearIntFlag();
+  //  CloseCapture1();
     // clear the timer interrupt flag
-    mIC1ClearIntFlag();
+  
 }
 
-void __ISR(_INPUT_CAPTURE_2_VECTOR, ipl4) C1Handler(void)
+void __ISR(_INPUT_CAPTURE_2_VECTOR, ipl4) C2Handler(void)
 {
     // read the capture register 
     time2 = mIC2ReadCapture();
-    CloseCapture2();
+     mIC2ClearIntFlag();
+   // CloseCapture2();
 
     // clear the timer interrupt flag
-    mIC2ClearIntFlag();
+   
 }
 
 
-void __ISR(_INPUT_CAPTURE_5_VECTOR, ipl5) C1Handler(void)
-{
+void __ISR(_INPUT_CAPTURE_5_VECTOR, ipl5) C5Handler(void)
+{       
     // read the capture register 
     time3 = mIC5ReadCapture();
-    CloseCapture5();
+     mIC5ClearIntFlag();
+   // CloseCapture5();
 
     // clear the timer interrupt flag
-    mIC5ClearIntFlag();
+   
+}
+
+void __ISR(_INPUT_CAPTURE_4_VECTOR, ipl2) C4Handler(void)
+{       
+    // read the capture register 
+     mIC4ClearIntFlag();
+         mIC4ReadCapture();
+
+   // CloseCapture5();
+//WriteTimer3(0x000);
+    // clear the timer interrupt flag
+   
 }
 void main(){
 
@@ -228,41 +246,48 @@ void main(){
 
   // === setup system wide interrupts  ========
 INTEnableSystemMultiVectoredInt();
-timerlimit = SYS_FREQ* PWM_Period/1000;
-pwm_on_time_idle=1.5*SYS_FREQ/1000;  
-pwm_on_time_max=1.7*SYS_FREQ/1000;
-pwm_on_time_min=1.3*SYS_FREQ/1000;
+timerlimit = SYS_FREQ* PWM_Period/256000;
+pwm_on_time_idle=1.5*SYS_FREQ/256000;  
+pwm_on_time_max=1.7*SYS_FREQ/256000;
+pwm_on_time_min=1.3*SYS_FREQ/256000;
 tdoa1_threshold=SYS_FREQ/100000;
 
-OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_1, timerlimit);
+OpenTimer2(T2_ON | T2_SOURCE_INT | T2_PS_1_256, timerlimit);
 OpenTimer3(T3_ON | T3_SOURCE_INT | T3_PS_1_1, 0xffff);
 
     // Need ISR to compute PID controller 
-ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2); 
-    // set up compare3 for PWM mode 
+ConfigIntTimer2(T2_INT_ON | T2_INT_PRIOR_2);     // set up compare3 for PWM mode 
 OpenOC3(OC_ON | OC_TIMER2_SRC | OC_PWM_FAULT_PIN_DISABLE , pwm_on_time_wheel1, pwm_on_time_wheel1);
 OpenOC2(OC_ON | OC_TIMER2_SRC | OC_PWM_FAULT_PIN_DISABLE , pwm_on_time_wheel2, pwm_on_time_wheel2);
 // OC3 is PPS group 4, map to RPB9 (pin 18) 
 PPSOutput(4, RPB9, OC3);// disconnect port expander
 PPSOutput(2, RPB8, OC2);
 
-
-  // turn on the interrupt so that every capture can be recorded
-ConfigIntCapture1(IC_INT_ON | IC_INT_PRIOR_3 | IC_INT_SUB_PRIOR_3 );
+OpenCapture1(  IC_EVERY_RISE_EDGE | IC_INT_1CAPTURE | IC_TIMER3_SRC | IC_ON );
+   OpenCapture2(  IC_EVERY_RISE_EDGE | IC_INT_1CAPTURE | IC_TIMER3_SRC | IC_ON );
+   OpenCapture5(  IC_EVERY_RISE_EDGE | IC_INT_1CAPTURE | IC_TIMER3_SRC | IC_ON );
+    OpenCapture4(  IC_EVERY_FALL_EDGE | IC_INT_1CAPTURE | IC_TIMER3_SRC | IC_ON );
+   ConfigIntCapture1(IC_INT_ON | IC_INT_PRIOR_1 | IC_INT_SUB_PRIOR_3 );
 INTClearFlag(INT_IC1);
     
-ConfigIntCapture2(IC_INT_ON | IC_INT_PRIOR_3 | IC_INT_SUB_PRIOR_3 );
+ConfigIntCapture2(IC_INT_ON | IC_INT_PRIOR_2 | IC_INT_SUB_PRIOR_3 );
 INTClearFlag(INT_IC2);
     
 ConfigIntCapture5(IC_INT_ON | IC_INT_PRIOR_3 | IC_INT_SUB_PRIOR_3 );
 INTClearFlag(INT_IC5);
+ConfigIntCapture4(IC_INT_ON | IC_INT_PRIOR_4 | IC_INT_SUB_PRIOR_3 );
+INTClearFlag(INT_IC4);
+  // turn on the interrupt so that every capture can be recorded
+
   // connect PIN 24 to IC1 capture unit
 PPSInput(3, IC1, RPA4);
 PPSInput(4, IC2, RPA3);
 PPSInput(3, IC5, RPA2);
+//PPSInput(2, INT3, RPA1);
+PPSInput(1, IC4, RPB3);
 
-ConfigINT3(EXT_INT_PRI_7 | FALLING_EDGE_INT | EXT_INT_ENABLE);
-PPSInput(2, INT3, RPA1);
+//ConfigINT3(EXT_INT_PRI_7 | FALLING_EDGE_INT | EXT_INT_ENABLE);
+
 int k;
     for(k=0;k<10;k++){
         tdoa1[k]=0;
